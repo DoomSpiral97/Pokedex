21913ff382e9e9610e417c1059c57c9fd5d4f11a
KubbutatNicholas <KubbutatNicholas@NB-5CG9513Q7D>
2026-04-24 14:45:21 +0200

    Claude hat geregelt... IconTyp Converter

---
+++ Pokedex/Converters/TypeIconConverter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace Pokedex.Converters
+{
+    public class TypeIconConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is string typeName)
+            {
+                // "Fire" → "pack://application:,,,/Media/Icons/Icon Fire.png"
+                return $"pack://application:,,,/Media/Icons/Icon {typeName}.png";
+            }
+            return string.Empty;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
